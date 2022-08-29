41c36834f72c89ca34d59e69b723b98d3cf44da0
nolann7 <nolann709@gmail.com>
2022-08-29 19:33:04 +0300

    adding js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+// current year in footer
+const currentYear = new Date().getFullYear();
+const yearEl = document.querySelector('.year');
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////
+const headerEl = document.querySelector('.header');
+const mobileMenuBtnOpenEl = document.querySelector('.btn-mobile-nav');
+mobileMenuBtnOpenEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
